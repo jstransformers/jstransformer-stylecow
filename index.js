ebd9648a15f71b8ddceb11d74ffa748100fb72f1
Rob Loach <robloach@gmail.com>
2015-06-28 18:37:42 -0400

    Initial commit

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 'use strict';
 
-exports.name = 'foo';
-exports.inputFormats = ['foo', 'foobar'];
-exports.outputFormat = 'html';
+var stylecow = require('stylecow-core');
 
-exports.render = function (str, options) {
-  return str;
+exports.name = 'stylecow';
+exports.outputFormat = 'css';
+
+function processOptions(options) {
+  if (options.support) {
+    stylecow.minSupport(options.support);
+  }
+
+  if (options.plugins) {
+    options.plugins.forEach(function (plugin) {
+      stylecow.loadNpmModule('stylecow-plugin-' + plugin);
+    });
+  }
+
+  if (options.modules) {
+    options.modules.forEach(function (module) {
+      stylecow.loadNpmModule(module);
+    });
+  }
 }
+
+exports.render = function (str, options, locals) {
+  processOptions(options);
+  var css = stylecow.parse(str);
+  stylecow.run(css);
+  return css.toString();
+};
+
+exports.renderFile = function (file, options, locals) {
+  locals = locals || {}
+  processOptions(options);
+  var css = stylecow.parseFile(file);
+  stylecow.run(css)
+  return css.toString();
+};
